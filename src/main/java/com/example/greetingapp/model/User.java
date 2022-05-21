9636dd9d2298d8ac5509572f379cb7ad0599e3e8
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-05-20 19:51:00 -0700

    Controller uses Service to display data

---
+++ src/main/java/com/example/greetingapp/model/User.java
@@ -0,0 +1,25 @@
+package com.example.greetingapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    private String firstName;
+    private String lastName;
+//    private String email;
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+}
--- src/main/java/com/example/greetingapp/service/GreetingService.java
+++ src/main/java/com/example/greetingapp/service/GreetingService.java
@@ -5,7 +5,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 
-    public String getGreeting() {
+    public String getMessage() {
         return "Hello world !!";
+    }
+//Getting message of First and Last Name
+    public String getGreetingMessage(String fName, String lName) {
+        return "Hello " + fName + " " + lName;
+    }
+
+    public String postGreetingMessage(String fName, String lName) {
+        return "Hello " + fName + " " + lName;
     }
 }
